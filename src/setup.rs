e4f56df944d67cbdf9315997c2acffea22acba30
mikedelcastillo <johnmichaeldc@gmail.com>
2023-10-06 21:38:26 +0800

    refactor: use preset values

--- src/setup.rs
+++ src/setup.rs
@@ -4,7 +4,7 @@ use bevy_prototype_lyon::prelude::ShapePlugin;
 pub struct IronWildsSetupPlugin;
 impl Plugin for IronWildsSetupPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
+        app.insert_resource(ClearColor(Color::BLACK))
             .add_plugins(DefaultPlugins.set(LogPlugin {
                 filter: "info,wgpu_core=off,wgpu_hal=off".into(),
                 level: bevy::log::Level::DEBUG,
@@ -28,8 +28,5 @@ fn say_hello() {
 }
 
 fn spawn_camera(mut commands: Commands) {
-    commands.spawn(Camera2dBundle {
-        transform: Transform::from_xyz(0.0, 0.0, 0.0),
-        ..default()
-    });
+    commands.spawn(Camera2dBundle::default());
 }
