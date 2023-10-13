32bbf7436516632611e99611e0f42185ac809678
Mike del Castillo <johnmichaeldc@gmail.com>
2023-10-13 18:55:21 +0800

    feat: follow player with camera (#12)

--- src/plugins/player.rs
+++ src/plugins/player.rs
@@ -1,10 +1,11 @@
 use bevy::prelude::*;
 
-use crate::systems::player::spawn_player;
+use crate::systems::player::{follow_player, spawn_player};
 
 pub struct IronWildsPlayerPlugin;
 impl Plugin for IronWildsPlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_player);
+        app.add_systems(Startup, spawn_player)
+            .add_systems(Update, follow_player);
     }
 }
