9fca1452e825b97364af0c26a075caf66de0b482
mikedelcastillo <johnmichaeldc@gmail.com>
2023-10-14 18:17:24 +0800

    refactor: use is_changed for logging inventory

--- src/plugins/inventory.rs
+++ src/plugins/inventory.rs
@@ -1,7 +1,6 @@
 use bevy::prelude::*;
 
 use crate::{
-    events::inventory::LogInventoryEvent,
     resources::inventory::Inventory,
     systems::{
         inputs::choose_active_slot_keyboard,
@@ -14,7 +13,6 @@ impl Plugin for IronWildsInventoryPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<Inventory>()
             .register_type::<Inventory>()
-            .add_event::<LogInventoryEvent>()
             .add_systems(Update, pick_up_ground_items)
             .add_systems(Update, log_inventory)
             .add_systems(Update, choose_active_slot_keyboard);
