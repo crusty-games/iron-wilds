043d0b2fb9cea707e5dcbb5104734e60cd7054f8
Mike del Castillo <johnmichaeldc@gmail.com>
2023-10-19 10:43:30 +0800

    fix: items disappearing in inventory ui (#22)

--- src/plugins/ui.rs
+++ src/plugins/ui.rs
@@ -1,11 +1,12 @@
 use bevy::prelude::*;
 
+use crate::systems::inventory::pick_up_ground_items;
 use crate::systems::ui::{inventory_ui, spawn_inventory_ui};
 
 pub struct IronWildsUiPlugin;
 impl Plugin for IronWildsUiPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, spawn_inventory_ui)
-            .add_systems(Update, inventory_ui);
+            .add_systems(Update, inventory_ui.after(pick_up_ground_items));
     }
 }
